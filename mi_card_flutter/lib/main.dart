46743eee1752e61c07142263d77602772823809c
Wisara8 <33183269+Wisara8@users.noreply.github.com>
2020-03-22 18:57:57 -0700

    Added starting files via upload

---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+dsfjfhgs
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+    ),
+  );
+}
