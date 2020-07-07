f4444a925d239940356ccc45b6ebb39df6bf83de
Wisara8 <33183269+Wisara8@users.noreply.github.com>
2020-07-07 14:35:03 -0700

    Row and Column exercise

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -9,32 +9,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.lightBlue,
+          backgroundColor: Colors.teal,
           body: SafeArea(
-            child: Column(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
                   Container(
-                      height: 100.0,
-                      width: 100.0,
-//                margin: EdgeInsets.fromLTRB(20, 10, 50, 60),
-//                margin: EdgeInsets.all(20),
-//                  margin: EdgeInsets.only(left: 30),
-//                  padding: EdgeInsets.all(20),
-//                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-                      color: Colors.white,
-                      child: Text("Container 1")),
-                  SizedBox(height: 30.0),
-                  Container(
-                      height: 100.0,
                       width: 100.0,
                       color: Colors.red,
-                      child: Text("Container 2")),
+                      child: Text("Container 1")),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                            height: 100.0,
+                            width: 100.0,
+                            color: Colors.yellow,
+                            child: Text("Container 2")),
+                        Container(
+                            height: 100.0,
+                            width: 100.0,
+                            color: Colors.green,
+                            child: Text("Container 3")),
+                      ]),
                   Container(
-                      height: 100.0,
                       width: 100.0,
-                      color: Colors.teal,
-                      child: Text("Container 3")),
+                      color: Colors.blue,
+                      child: Text("Container 4")),
                 ]),
           )),
     );
