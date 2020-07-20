c00af8b63b2da35e9f190d4f3a772bfcd047988e
Wisara8 <33183269+Wisara8@users.noreply.github.com>
2020-07-19 17:13:00 -0700

    Added Card and ListTile Widgets.

--- mi_card_flutter/main.dart
+++ mi_card_flutter/main.dart
@@ -11,67 +11,64 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
-              child: Column(children: <Widget>[
-            CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/profile.png')),
-            Text("Samuel Randall",
-                style: TextStyle(
-                    fontFamily: "Pacifico",
-                    fontSize: 40.0,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage('images/profile.png')),
+                Text("Samuel Randall",
+                    style: TextStyle(
+                        fontFamily: "Pacifico",
+                        fontSize: 40.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
+                Text("WEB DEVELOPER",
+                    style: TextStyle(
+                      fontFamily: "SourceSansPro",
+                      fontSize: 20.0,
+                      color: Colors.teal.shade100,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
                     color: Colors.white,
-                    fontWeight: FontWeight.bold)),
-            Text("WEB DEVELOPER",
-                style: TextStyle(
-                  fontFamily: "SourceSansPro",
-                  fontSize: 20.0,
-                  color: Colors.teal.shade100,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                )),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text("1-778-847-8497",
-                      style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: "Source Sans Pro",
-                          fontSize: 20.0))
-                ],
-              ),
-            ),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text("srandall845@gmail.com",
-                      style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: "Source Sans Pro",
-                          fontSize: 20.0))
-                ],
-              ),
-            ),
-          ]))),
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text("1-778-847-8497",
+                          style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: "Source Sans Pro",
+                              fontSize: 20.0)),
+                    )),
+                Card(
+                    color: Colors.white,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text("srandall845@gmail.com",
+                          style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: "Source Sans Pro",
+                              fontSize: 20.0)),
+                    )),
+              ]))),
     );
   }
 }
